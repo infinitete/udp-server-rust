76f659aa7890c63bff9bf0d053e61055dd92b51d
adogwithfragrancy <adogwithfragrancy@gmail.com>
2018-12-17 14:51:26 +0800

    :fire: make a fix

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -7,10 +7,21 @@ pub fn run(ip: [u8; 4], port: u16) {
     let socket_addr = SocketAddrV4::new(addr, port);
 
     let socket = UdpSocket::bind(socket_addr).expect("Error");
+    let mut last_buff = [0;32];
 
     loop {
         let mut buf = [0;32];
-        let (len, src) = socket.recv_from(&mut buf).expect("Receve failure");
+        let (len, src) = socket.peek_from(&mut buf).expect("Receve failure");
+        let (_, _) = socket.recv_from(&mut last_buff).expect("Receve failure");
+
+        let is_fix = &buf[0..4];
+
+        if (is_fix.eq(&[9;4])) {
+            last_buff.reverse();
+            socket.send_to(&last_buff, src).expect("Send failure");
+            continue;
+        }
+
         println!("Receve from {}: {} - {:?}", &src.ip(), len, &buf);
         buf.reverse();
         socket.send_to(&buf, src).expect("Send failure");
